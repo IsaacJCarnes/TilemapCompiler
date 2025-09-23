df86083adaa948a2712713975cec0c2f5494b018
IsaacJ <isaacjdeveloper@gmail.com>
2025-09-22 22:07:38 -0400

    Added refresh button

--- Scripts/control_listener.gd
+++ Scripts/control_listener.gd
@@ -2,14 +2,15 @@ extends Node
 @export var image_displayer:Node
 @export var scaleScroll:HScrollBar
 @export var scrollTarget:TextureRect
+@export var refreshButton:Button
 
 func _ready() -> void:
 	scrollTarget.draw.connect(initScrollBar)
+	refreshButton.pressed.connect(image_displayer.load_images)
 
 func initScrollBar():
 	scrollTarget.draw.disconnect(initScrollBar)
 	var minVal = min(scrollTarget.size.x, scrollTarget.size.y)
-	print("hi ", minVal)
 	scaleScroll.value = minVal
 	scaleScroll.min_value = minVal
 	scaleScroll.step = ceil(ceil(minVal)/100)
